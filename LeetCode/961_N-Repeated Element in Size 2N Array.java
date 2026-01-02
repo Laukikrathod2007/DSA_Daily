ac6928de466544f15cbb67e9e880e7c23ce1549c
Laukikrathod2007 <rathodlaukik184@gmail.com>
2026-01-02 22:30:24 +0530

    Day 2

---
+++ LeetCode/961_N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,16 @@
+//961. N-Repeated Element in Size 2N Array
+//2 jan
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] == nums[i+1])
+            return nums[i];
+
+            if (i+2 < n)
+                if(nums[i] == nums[i+2])
+                return nums[i];
+        }
+        return nums[0];
+    }
+}
